b19a8e2631c8405e50008f81713b75c20375bd34
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-12-14 23:22:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function mapLetters(str){
+	const ans = {}
+	for (let i = 0; i < str. length; i++) {
+	    if (ans [str[i]]) {
+			ans[str[i]].push[i]
+		}
+		else{
+			ans[str[i]] = [i]
+		}
+	}
+	return ans;
+}
+const text = prompt("Enter text:");
+alert(toCase(text));
