61ec8d115d7da16e13fef24cd5826361ff2cd8e7
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-12-14 23:25:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 //your JS code here. If required.
-function mapLetters(str){
-	const ans = {}
-	for (let i = 0; i < str. length; i++) {
-	    if (ans [str[i]]) {
-			ans[str[i]].push[i]
-		}
-		else{
-			ans[str[i]] = [i]
-		}
-	}
-	return ans;
+function mapLetters(str) {
+  // Initialize an empty object to store the letter indices
+  const letterIndices = {};
+  // Iterate over the input string
+  for (let i = 0; i < str.length; i++) {
+    // Get the current character
+    const char = str[i];
+    // Check if the character is already a key in the object
+    if (!letterIndices[char]) {
+      // If not, add it as a key with an empty array as the value
+      letterIndices[char] = [];
+    }
+    // Append the current index to the array associated with the character
+    letterIndices[char].push(i);
+  }
+  // Return the object with the letter indices
+  return letterIndices;
 }
-const text = prompt("Enter text:");
-alert(toCase(text));
+
